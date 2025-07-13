56275179498a19a997820833666d8ed2e94c54b1
Muhammed Musthafa <MdMusthafapp@gmail.com>
2025-07-13 21:47:14 +0530

    updated package

--- Package.swift
+++ Package.swift
@@ -1,5 +1,28 @@
 
 // swift-tools-version:5.5
+// import PackageDescription
+
+// let package = Package(
+//     name: "OpenSSL",
+//     platforms: [
+//         .iOS(.v12),
+//         .macOS(.v10_15)
+//     ],
+//     products: [
+//         .library(
+//             name: "OpenSSL",
+//             targets: ["OpenSSLBinary"]
+//         ),
+//     ],
+//     targets: [
+//         .binaryTarget(
+//             name: "OpenSSLBinary",
+//             url: "https://github.com/ios-poc/OpenSSL-SPM/releases/download/0.0.6/OpenSSL.xcframework.zip",
+//             checksum: "221eabb03ecfe09c1130a2aa30ade835971192d86be76ff9cf676a781c71c710"
+//         )
+//     ]
+// )
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
@@ -11,14 +34,20 @@ let package = Package(
     products: [
         .library(
             name: "OpenSSL",
-            targets: ["OpenSSLBinary"]
+            targets: ["OpenSSL"]
         ),
     ],
     targets: [
+        .target(
+            name: "OpenSSL",
+            dependencies: ["OpenSSLBinary"],
+            path: "Sources/OpenSSL",  // to resolve the OpenSSL.h header
+            publicHeadersPath: "."
+        ),
         .binaryTarget(
             name: "OpenSSLBinary",
-            url: "https://github.com/ios-poc/OpenSSL-SPM/releases/download/0.0.6/OpenSSL.xcframework.zip",
-            checksum: "221eabb03ecfe09c1130a2aa30ade835971192d86be76ff9cf676a781c71c710"
+            path: "OpenSSL.xcframework"
         )
     ]
 )
+
