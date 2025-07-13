0a24de0541cc89e5f0989582f628b486367844e7
Muhammed Musthafa <MdMusthafapp@gmail.com>
2025-07-13 14:45:38 +0530

    tag 6

--- Package.swift
+++ Package.swift
@@ -1,3 +1,4 @@
+
 // swift-tools-version:5.5
 import PackageDescription
 
@@ -10,19 +11,14 @@ let package = Package(
     products: [
         .library(
             name: "OpenSSL",
-            targets: ["OpenSSL"]
+            targets: ["OpenSSLBinary"]
         ),
     ],
     targets: [
-        .target(
-            name: "OpenSSL",
-            dependencies: ["OpenSSLBinary"]
-        ),
         .binaryTarget(
-          name: "OpenSSLBinary",
-          url: "https://github.com/ios-poc/OpenSSL-SPM/releases/download/0.0.5/OpenSSL.xcframework.zip",
-          checksum: "221eabb03ecfe09c1130a2aa30ade835971192d86be76ff9cf676a781c71c710"
+            name: "OpenSSLBinary",
+            url: "https://github.com/ios-poc/OpenSSL-SPM/releases/download/0.0.6/OpenSSL.xcframework.zip",
+            checksum: "221eabb03ecfe09c1130a2aa30ade835971192d86be76ff9cf676a781c71c710"
         )
     ]
 )
-//https://github.com/ios-poc/OpenSSL-SPM/releases/tag/0.0.2#:~:text=OpenSSL.xcframework.zip
